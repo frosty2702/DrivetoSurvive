361ed5001eaa8a54c50aae7a8c83ef4fdd957df8
Vishal2704 <vishalrajendran27@gmail.com>
2025-10-24 23:47:16 +0530

    more files

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeamDashboard from './components/TeamDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TeamDashboard />
+    </div>
+  );
+}
+
+export default App;
