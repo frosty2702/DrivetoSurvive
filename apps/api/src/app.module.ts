14e05961e1a41e1c2d5ce47e3b1b904f04181051
Vishal2704 <vishalrajendran27@gmail.com>
2025-10-25 01:41:51 +0530

    http integ

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { DriversModule } from './drivers/drivers.module';
 import { TestModule } from './test/test.module';
+import { F1DataModule } from './f1-data/f1-data.module';
 
 @Module({
-  imports: [PrismaModule, DriversModule, TestModule],
+  imports: [
+    HttpModule.register({
+      timeout: 10000,
+      maxRedirects: 5,
+    }),
+    PrismaModule, 
+    DriversModule, 
+    TestModule,
+    F1DataModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
